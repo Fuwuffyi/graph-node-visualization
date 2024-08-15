e9a54083ecf2977b479b7b1ebf7d8410f58f6233
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-08-15 23:23:16 +0200

    Add base Node class for graphing

--- include/node.hpp
+++ include/node.hpp
@@ -1,3 +1,26 @@
 #pragma once
 
-class Node {};
+#include <glm/glm.hpp>
+#include <string>
+
+class Node {
+public:
+  Node(const std::string &name_, const float mass_);
+  Node(const std::string &name_);
+
+  void applyForce(const glm::vec2 &force, const float deltaTime);
+  void update(const float velocityDamping); // Damping must be 0 .. 1
+
+  const std::string &getName() const;
+  const float &getMass() const;
+  const glm::vec2 &getPosition() const;
+  const glm::vec2 &getVelocity() const;
+  const glm::vec2 &getAcceleration() const;
+
+private:
+  const std::string name;
+  const float mass;
+  glm::vec2 position;
+  glm::vec2 velocity;
+  glm::vec2 acceleration;
+};
--- src/node.cpp
+++ src/node.cpp
@@ -1,1 +1,24 @@
 #include "../include/node.hpp"
+
+Node::Node(const std::string &name_, const float mass_)
+    : name(name_), mass(mass_), position(0.0f), velocity(0.0f),
+      acceleration(0.0f) {}
+
+Node::Node(const std::string &name_) : Node(name_, 1.0f) {}
+
+void Node::applyForce(const glm::vec2 &force, const float deltaTime) {
+  acceleration += force / mass * deltaTime;
+}
+
+void Node::update(const float velocityDamping) {
+  velocity += acceleration;
+  velocity *= velocityDamping;
+  position += velocity;
+  acceleration = glm::vec2(0.0f);
+}
+
+const std::string &Node::getName() const { return name; }
+const float &Node::getMass() const { return mass; }
+const glm::vec2 &Node::getPosition() const { return position; }
+const glm::vec2 &Node::getVelocity() const { return velocity; }
+const glm::vec2 &Node::getAcceleration() const { return acceleration; }
