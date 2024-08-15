87723b43082ef3b61290d33b20fb3336644a7f8d
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-08-15 23:28:33 +0200

    Add base Link class

--- include/link.hpp
+++ include/link.hpp
@@ -1,3 +1,15 @@
 #pragma once
 
-class Link {};
+#include "node.hpp"
+
+class Link {
+public:
+  Link(const Node &nodeA_, const Node &nodeB_);
+
+  const Node &getNodeA() const;
+  const Node &getNodeB() const;
+
+private:
+  const Node &nodeA;
+  const Node &nodeB;
+};
--- src/link.cpp
+++ src/link.cpp
@@ -1,1 +1,8 @@
 #include "../include/link.hpp"
+
+Link::Link(const Node &nodeA_, const Node &nodeB_)
+    : nodeA(nodeA_), nodeB(nodeB_) {}
+
+const Node &Link::getNodeA() const { return nodeA; }
+
+const Node &Link::getNodeB() const { return nodeB; }
