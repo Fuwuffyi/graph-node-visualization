4d7676ed4007098b4428fee5194a4b46493246be
Palazzini Luca <luca.palazzini3@studio.unibo.it>
2024-08-15 22:59:48 +0200

    Add base window class

--- include/window.hpp
+++ include/window.hpp
@@ -1,1 +1,15 @@
 #pragma once
+
+#include <SDL2/SDL.h>
+#include <string>
+
+class Window {
+public:
+  Window(const std::string &title, const uint32_t width, const uint32_t height);
+  ~Window();
+
+private:
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+  bool isRunning;
+};
--- src/window.cpp
+++ src/window.cpp
@@ -1,1 +1,39 @@
 #include "../include/window.hpp"
+
+#include <iostream>
+
+Window::Window(const std::string &title, const uint32_t width,
+               const uint32_t height)
+    : window(nullptr), renderer(nullptr), isRunning(true) {
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    std::cerr << "SDL could not initialize! SDL_Error: " << SDL_GetError()
+              << std::endl;
+    isRunning = false;
+    return;
+  }
+  window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED,
+                            SDL_WINDOWPOS_CENTERED, static_cast<int32_t>(width),
+                            static_cast<int32_t>(height), SDL_WINDOW_SHOWN);
+  if (window == nullptr) {
+    std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError()
+              << std::endl;
+    isRunning = false;
+    SDL_Quit();
+    return;
+  }
+  renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  if (renderer == nullptr) {
+    std::cerr << "Renderer could not be created! SDL_Error: " << SDL_GetError()
+              << std::endl;
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    isRunning = false;
+    return;
+  }
+}
+
+Window::~Window() {
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+}
