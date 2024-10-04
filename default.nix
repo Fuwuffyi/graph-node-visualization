4645c2b9057f1328408416d93b5203ce799b0a94
Fuwuffy <palazzini21@gmail.com>
2024-10-04 16:02:08 +0200

    Remove a nix shell dependency

--- default.nix
+++ default.nix
@@ -19,8 +19,4 @@ clangStdenv.mkDerivation {
     pkgs.SDL2
     pkgs.SDL2_ttf
   ];
-
-  shellHook = ''
-    export NIX_LD=$(nix eval --impure --raw --expr 'let pkgs = import <nixpkgs> {}; NIX_LD = pkgs.lib.fileContents "${pkgs.stdenv.cc}/nix-support/dynamic-linker"; in NIX_LD')
-  '';
 }
